b4166166c6963bae8f1204992843f591b707e9e6
Jay Gurnani <jay.gurnani@gmail.com>
2022-07-27 22:38:16 +1000

    Adding testing scaffolding

---
+++ CheckoutInterview.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckoutInterview.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
